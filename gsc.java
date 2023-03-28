011f70f5d3064203f1f56f2297c7d59a7aa7364c
Your Name <you@example.com>
2023-03-28 14:08:42 +0600

    I have added a java file containing getter, setter and constructor

---
+++ gsc.java
@@ -0,0 +1,30 @@
+class MyEmployee{
+    private int id;
+    private String name;
+    public void setId(int i){
+        id=i;
+    }
+    public void setName(String n){
+        name=n;
+    }
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+    MyEmployee(int i1,String n1){
+        id=i1;
+        name=n1;
+    }
+}
+public class gsc {
+    public static void main(String[] args) {
+        MyEmployee x,y;
+        x=new MyEmployee();
+        x.setId(1);
+        x.setName("Getter&Setter");
+        System.out.println(x.getId());
+        System.out.println(x.getName());
+    }
+}
